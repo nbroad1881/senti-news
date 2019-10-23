73c3d62c3964d0032d8bed330d9d5ecfaf0bf1e0
Nicholas Broad <nbroad94@gmail.com>
2019-10-23 16:58:39 -0400

    removed pickle

--- scrapy_news/nyt_api.py
+++ scrapy_news/nyt_api.py
@@ -1,21 +1,13 @@
 import requests
 import json
-import pickle
 
 """Documentation
 https://developer.nytimes.com/docs/articlesearch-product/1/routes/articlesearch.json/get
 """
-url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=biden|sanders|warren|buttigieg|harris&api-key=nSc6ri8B5W6boFhjJ6SuYpQmLN8zQuV7'
+url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=biden|sanders|warren|buttigieg|harris&api-key' \
+      '=nSc6ri8B5W6boFhjJ6SuYpQmLN8zQuV7 '
 r = json.loads(requests.get(url).text)
 
-save = False
-if save:
-    with open('nyt_response', 'wb') as f:
-        pickle.dump(r, f)
-read = False
-if read:
-    with open('nyt_response', 'rb') as f:
-        r = pickle.load(f)
 print(r['response']['meta'])
 
 # Response structure
