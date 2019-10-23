eb3a8163da7f204637c8a8fced55f0aa3c3bb964
Nicholas Broad <nbroad94@gmail.com>
2019-10-23 14:25:02 -0400

    reformatted fox_spider.py, news_url_getter.py renamed

--- scripts/news_url_getter.py
+++ scripts/news_url_getter.py
@@ -3,20 +3,20 @@ import csv
 import json
 
 with open('config.json', 'r') as j_file:
-	j = json.load(j_file)
-	key = j['API-Key']
+    j = json.load(j_file)
+    key = j['API-Key']
 
 newsapi = NewsApiClient(api_key=key)
 
 candidates = ['Joe Biden',
-				'Elizabeth Warren',
-				'Bernie Sanders',
-				'Pete Buttigieg',
-				'Kamala Harris']
+              'Elizabeth Warren',
+              'Bernie Sanders',
+              'Pete Buttigieg',
+              'Kamala Harris']
 
 srcs = ['the-new-york-times',
-			'cnn',
-			'fox-news']
+        'cnn',
+        'fox-news']
 
 url_dict = {}
 
@@ -26,23 +26,22 @@ dt = '2019-09-3'
 
 urls = set()
 
+for n in range(0, 1, 1):
+    for c in candidates:
+        all_articles = newsapi.get_everything(q=c, from_param=dt + str(n), to=dt + str(n),
+                                              sources=','.join(srcs), page_size=100)
 
-for n in range(0,1,1):
-	for c in candidates:
-		all_articles = newsapi.get_everything(q=c,from_param=dt+str(n),to=dt+str(n),
-								sources=','.join(srcs), page_size=100)
+        total_results = all_articles['totalResults']
+        print(dt + str(n), '- total results for', c, '=', total_results)
+        urls.update([a['url'] for a in all_articles['articles']])
 
-		total_results = all_articles['totalResults']
-		print(dt+str(n),'- total results for',c,'=',total_results)
-		urls.update([a['url'] for a in all_articles['articles']])
-
-		# for p in range(2,total_results//100+1,1):
-		# 	all_articles = newsapi.get_everything(q=c,from_param=start_date,to=end_date,
-		# 						sources=','.join(srcs), page_size=100, page=p)
-		# 	urls.update([a['url'] for a in all_articles['articles']])
-		# print(p)
-	filename = 'urls/all-'+dt+str(n)+'.csv'
-	with open(filename,'w') as csvfile:
-		writer = csv.writer(csvfile)
-		writer.writerow(list(urls))
-	print("final count =",len(urls))
+    # for p in range(2,total_results//100+1,1):
+    # 	all_articles = newsapi.get_everything(q=c,from_param=start_date,to=end_date,
+    # 						sources=','.join(srcs), page_size=100, page=p)
+    # 	urls.update([a['url'] for a in all_articles['articles']])
+    # print(p)
+    filename = 'urls/all-' + dt + str(n) + '.csv'
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(list(urls))
+    print("final count =", len(urls))
