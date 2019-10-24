4233201da25bd5091a1470bcdae08c316be72b86
Nicholas Broad <nbroad94@gmail.com>
2019-10-24 11:05:36 -0400

    added constructor that accepts start_urls, wrote method to pull urls and info from api response from fox

--- scrapy_news/scraper.py
+++ scrapy_news/scraper.py
@@ -5,45 +5,46 @@ import datetime
 import sys
 import requests
 
+
 # todo: have an interactive query
 #     database for text documents
 class NewsSpider(scrapy.Spider):
     """Spider to grab news text
     """
     name = 'scrape-news'
-    print(sys.argv)
-    start_urls = [
-        'https://search.api.cnn.io/content?size=100&q=biden%20%7C%20sanders%20%7C%20warren%20%7C%20buttigieg%20%7C'
-        '%20harris&category=us&type=article&sort=newest',
-        'https://api.foxnews.com/v1/content/search?q=buttigieg%20|%20harris%20|%20sanders&ss=fn&section.path=fnc'
-        '/politics&type=story&min_date=2019-10-22&max_date=2019-10-23&start=0&callback=angular.callbacks._0&cb'
-        '=2019101792',
 
-    ]
+    def __init__(self, start_urls=[]):
+        super().__init__()
+        self.start_urls = start_urls
+
+    # print(sys.argv)
+    # start_urls = [
+    #     'https://search.api.cnn.io/content?size=100&q=biden%20%7C%20sanders%20%7C%20warren%20%7C%20buttigieg%20%7C'
+    #     '%20harris&category=us&type=article&sort=newest',
+    #     'https://api.foxnews.com/v1/content/search?q=buttigieg%20|%20harris%20|%20sanders&ss=fn&section.path=fnc'
+    #     '/politics&type=story&min_date=2019-10-22&max_date=2019-10-23&start=0&callback=angular.callbacks._0&cb'
+    #     '=2019101792',
+    #
+    # ]
 
     def parse(self, response):
-        pass
-
-
-today = datetime.date.today().isoformat()
-
-
-process = CrawlerProcess()
-process.crawl(NewsSpider)
-process.start()
-
+        if('angular' in response.text[:30]):
+            fox_urls = get_fox_urls(response)
+            # scrape_fox(fox_urls)
+        print('HELLO'.center(50, '*'))
+        print(response.url)
 
 
 def form_fox_query(q, min_date, max_date, start):
     s_1 = 'https://api.foxnews.com/v1/content/search?q='
-    q = 'biden'
+    q = q
     s_2 = '&fields=date,description,title,url,image,type,taxonomy&section.path=fnc&type=article&min_date='
-    min_dt = '2019-01-01'
+    min_dt = min_date
     s_3 = '&max_date='
-    max_dt = '19-10-01'
+    max_dt = max_date
     s_4 = '&start='
-    start = 0
-    s_5 = '&callback=angular.callbacks._0&cb=2019101792'
+    start = str(start)
+    s_5 = '&callback=angular.callbacks._0&cb=20191024112'
     return ''.join([s_1, q, s_2, min_dt, s_3, max_dt, s_4, start, s_5])
 
 
@@ -62,11 +63,11 @@ def scrape_nyt(response):
 #   have support for date in the query
 def scrape_cnn():
     url = 'https://search.api.cnn.io/content?size=100&q=biden%20%7C%20sanders%20%7C%20warren%20%7C%20buttigieg%20%7C' \
-      '%20harris&category=us&type=article&sort=newest'
+          '%20harris&category=us&type=article&sort=newest'
     response = requests.get(url)
     if response.status_code == 200:
         articles = json.loads(response.text)['result']
-        with open('cnn_articles.txt','a') as f:
+        with open('cnn_articles.txt', 'a') as f:
             num_results = None
             for a in articles:
                 if num_results is None:
@@ -74,11 +75,36 @@ def scrape_cnn():
                 if a.type != 'article':
                     pass
                     # skip over
-                f.write(a['_id']+'\n')
-                f.write(a['body']+'\n')
-                f.write(a['url']+'\n')
-                f.write(a['firstPublishDate']+'\n')
-                f.write(a['headline']+'\n')
+                f.write(a['_id'] + '\n')
+                f.write(a['body'] + '\n')
+                f.write(a['url'] + '\n')
+                f.write(a['firstPublishDate'] + '\n')
+                f.write(a['headline'] + '\n')
 
 
+def get_fox_urls(response):
+    try:
+        text = response.text[21:]
+        if len(text) < 0:
+            print(f'Response was not usual length \n{text}')
+            return ''
+    except TypeError:
+        print(f'Response text was not a string object but of type {type(text)}')
+        return ''
+    res = json.loads(text)
+    n_results = res['numFound']
+    urls = [n_results]
+    for d in res['docs']:
+        dt = d['date']
+        title = d['title']
+        url = d['url']
+        urls.append((dt, title, url))
+    return urls
 
+
+today = datetime.date.today().isoformat()
+
+if __name__ == "__main__":
+    process = CrawlerProcess()
+    process.crawl(NewsSpider, start_urls=[form_fox_query(q='biden', '2019-01-01', '2019-10-10', start=0)])
+    process.start()
