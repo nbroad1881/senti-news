1be679798f1dda098c694dc8b2a9a3f290f01e34
Nicholas Broad <nbroad94@gmail.com>
2019-10-25 14:45:18 -0400

    feat: saves sentiment scores to txt file
    also prints progress

--- scripts/vader.py
+++ scripts/vader.py
@@ -1,10 +1,15 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import csv
 
-
 analyzer = SentimentIntensityAnalyzer()
-with open('../texts/cnn_articles.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        vs = analyzer.polarity_scores(row[4])
-        print(f"On {row[0][:10]}, the article '{row[1]}' has a sentiment score of {str(vs)}")
+with open('cnn_sentiment_scores.txt', 'w') as s:
+    s.write('positive sentiment: compound score >= 0.05, neutral sentiment: (compound score > -0.05) and (compound '
+            'score < 0.05), negative sentiment: compound score <= -0.05\n')
+    with open('../texts/cnn_articles.csv', 'r') as f:
+        reader = csv.reader(f)
+        counter = 1
+        for row in reader:
+            vs = analyzer.polarity_scores(row[4])
+            s.write(f"On {row[0][:10]}, the article '{row[1]}' has a sentiment score of {str(vs)}\n")
+            print(f'{counter} done', end="\r")
+            counter += 1
