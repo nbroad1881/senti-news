38404d322d7abec9ca605127fe6608a8419cee25
Nicholas Broad <nbroad94@gmail.com>
2019-10-28 12:23:04 -0400

    feat: grab string scores from file and turn into array of objects

--- scripts/vader.py
+++ scripts/vader.py
@@ -1,5 +1,6 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import csv
+import json
 
 def analyze_text(filepath):
     analyzer = SentimentIntensityAnalyzer()
@@ -14,3 +15,21 @@ def analyze_text(filepath):
                 s.write(f"On {row[0][:10]}, the article '{row[1]}' has a sentiment score of {str(vs)}\n")
                 print(f'{counter} done', end="\r")
                 counter += 1
+
+def aggregate_scores(filepath):
+
+    scores = []
+    with open(filepath,'r') as f:
+        headers = f.readline()
+        count = 0
+        for line in f:
+            score = line[line.index('{'):-1]
+            score = score.replace("'", '"')
+            scores.append(json.loads(score))
+            if count > 5: break
+            count += 1
+
+    print(scores)
+
+if __name__ == '__main__':
+    aggregate_scores('sentiment_scores/cnn_sentiment_scores_vader.txt')
