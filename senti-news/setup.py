920ffd99e5c3ffe90df1f3efc4fd013a219ca930
Nicholas Broad <nbroad94@gmail.com>
2019-12-04 11:53:31 -0500

    fix: add correct install_requirements and parameters to find_packages

--- senti-news/setup.py
+++ senti-news/setup.py
@@ -3,17 +3,36 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+install_requirements = [
+    'sqlalchemy',
+    'cytoolz',
+    'numpy',
+    'keras',
+    'spacy',
+    'textblob',
+    'vaderSentiment',
+    'pandas',
+    'newsapi-python',
+    'python-dateutil',
+    'requests',
+    'bs4',
+    'scrapy',
+    'tensorflow',
+    'python-dotenv',
+]
+
 setuptools.setup(
     name="senti-news",
-    version="0.0.12",
+    version="0.0.19",
     author="Nicholas Broad",
     author_email="nicholas@nmbroad.com",
     description="News title sentiment analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nbroad1881/senti-news",
-    packages=setuptools.find_packages(),
-    package_dir={'':'src'},
+    packages=setuptools.find_packages(where='src/'),
+    package_dir={'': 'src'},
+    install_requires=install_requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
