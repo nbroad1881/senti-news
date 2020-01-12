ff69ef15db1129ee9de8763df5f173a0b12f0871
Nicholas Broad <nbroad94@gmail.com>
2020-01-12 16:50:33 -0500

    feat: add NewsItemPipeline that takes NewsItems and puts them in the database

--- senti-news/src/sentinews/scraping/scraping/pipelines.py
+++ senti-news/src/sentinews/scraping/scraping/pipelines.py
@@ -1,3 +1,10 @@
+import logging
+
+from scrapy.exceptions import DropItem
+
+from sentinews.database import DataBase
+
+
 # -*- coding: utf-8 -*-
 
 # Define your item pipelines here
@@ -7,5 +14,39 @@
 
 
 class ScrapingPipeline(object):
+
     def process_item(self, item, spider):
         return item
+
+
+class NewsItemPipeline:
+
+    def __init__(self):
+        self.db = DataBase()
+
+    def open_spider(self, spider):
+        pass
+
+    def close_spider(self, spider):
+        self.db.close_session()
+        logging.info(f"Logged {spider.articles_logged} articles")
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls()
+
+    def process_item(self, item, spider):
+        result = self.db.add_row(
+            url=item['url'],
+            datetime=item['datetime'],
+            title=item['title'],
+            news_co=item['news_co'],
+            text=item['text']
+        )
+        if result:
+            result_string = 'successfully added'
+        else:
+            result_string = 'not added'
+
+        raise DropItem(f'{item["title"]} {result_string}')
+
