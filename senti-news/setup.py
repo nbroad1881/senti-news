7bdc6627bf19ff3e3afecd608ca106165d4fe917
Nicholas Broad <nbroad94@gmail.com>
2020-02-05 15:48:12 -0500

    feat: update version to 38

--- senti-news/setup.py
+++ senti-news/setup.py
@@ -19,7 +19,7 @@ install_requirements = [
 
 setuptools.setup(
     name="senti-news",
-    version="0.0.36",
+    version="0.0.38",
     author="Nicholas Broad",
     author_email="nicholas@nmbroad.com",
     description="News title sentiment analysis",
